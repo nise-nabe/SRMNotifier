ece44d50548fab2760385010c6cf0beb61286990
nise_nabe <nise.nabe@gmail.com>
2012-03-31 13:55:35 +0900

    add ParserTest file

---
+++ src/notifier/parser/SRMCalendarParserTest.java
@@ -0,0 +1,12 @@
+package notifier.parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SRMCalendarParserTest {
+	@Test
+	public void testName() throws Exception {
+
+	}
+}
