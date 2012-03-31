f63d5320cbad1844141a05c00229f234e10f03c4
nise_nabe <nise.nabe@gmail.com>
2012-03-31 13:44:52 +0900

    fixed to use SRMCalendarParser

--- src/notifier/SRMUpdaterServlet.java
+++ src/notifier/SRMUpdaterServlet.java
@@ -1,16 +1,12 @@
 package notifier;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
 import javax.jdo.Extent;
 import javax.jdo.PersistenceManager;
@@ -20,14 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import notifier.parser.SRMCalendarParser;
-
-import org.htmlparser.Node;
-import org.htmlparser.Parser;
-import org.htmlparser.filters.HasAttributeFilter;
-import org.htmlparser.filters.TagNameFilter;
-import org.htmlparser.tags.LinkTag;
-import org.htmlparser.util.NodeList;
-import org.htmlparser.util.SimpleNodeIterator;
 
 //履歴
 //2010/5/4 16:30 作成
@@ -64,7 +52,10 @@ public class SRMUpdaterServlet extends HttpServlet {
 	}
 
 	private void updateSRM(PersistenceManager pm, Date now, String month) {
-		List<SRM> updates = getSRMs(month);
+		SRMCalendarParser parser = new SRMCalendarParser(
+				"http://community.topcoder.com/tc?module=Static&d1=calendar&d2="
+						+ month);
+		List<SRM> updates = parser.getSRMs();
 		Extent<SRM> extent = pm.getExtent(SRM.class, true);
 		for (SRM srm : extent) {
 			log.info("格納済みデータ" + srm);
@@ -95,69 +86,6 @@ public class SRMUpdaterServlet extends HttpServlet {
 				}
 			}
 		}
-	}
-
-	private ArrayList<SRM> getSRMs(String month) {
-		ArrayList<SRM> result = new ArrayList<SRM>();
-		String url = "http://community.topcoder.com/tc?module=Static&d1=calendar&d2="
-				+ month;
-		try {
-			Parser parser = new Parser(url);
-			log.info("カレンダー取得 from " + url);
-			NodeList list = parser
-					.parse(new HasAttributeFilter("class", "srm"));
-			SimpleNodeIterator it = list.elements();
-			while (it.hasMoreNodes()) {
-				NodeList children = it.nextNode().getChildren();
-				children.keepAllNodesThatMatch(new TagNameFilter("a"));
-				Node a = children.elementAt(0);
-				if (a instanceof LinkTag) {
-					LinkTag link = (LinkTag) a;
-					SRM srm = new SRM();
-					srm.setName(link.toPlainTextString());
-					srm.setUrl(link.getLink());
-					List<Date> dates = getTimes(srm.getUrl());
-					srm.setRegisterTime(dates.get(0));
-					srm.setCompetisionTime(dates.get(1));
-					srm.setCount(0);
-					result.add(srm);
-					log.info("SRMデータ解析 : " + srm);
-				}
-			}
-		} catch (Exception e) {
-			log.warning(e.getMessage());
-		}
-		return result;
-	}
-
-	private List<Date> getTimes(String url) {
-		ArrayList<Date> dates = new ArrayList<Date>();
-		try {
-			Parser parser = new Parser(url);
-			NodeList list = parser.parse(new HasAttributeFilter("class",
-					"statText"));
-			SimpleNodeIterator it = list.elements();
-			String day = "";
-			while (it.hasMoreNodes()) {
-				Node node = it.nextNode();
-				String text = node.toPlainTextString().replaceAll("\\s", "");
-				if (Pattern.matches("..\\..+", text)) {
-					day = text;
-				} else {
-					// 02.17.201006:00PMEST "MM.dd.yyyyhh:mmaz"
-					// version5追記 EDTなるサマータイムが存在．でもjavaだから問題なかった．
-					SimpleDateFormat format2 = new SimpleDateFormat(
-							"MM.dd.yyyyhh:mmaz", Locale.US);
-					Date parse = format2.parse(day + text);
-					dates.add(parse);
-					log.info("データ取得 from " + url + ",date=" + day + text
-							+ " to " + parse + " and " + format.format(parse));
-
-				}
-			}
-		} catch (Exception e) {
-		}
-		return dates;
 	}
 
 }
