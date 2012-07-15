0cbb75ff07236ddfe4d50577ccbe2044b5979470
nise_nabe <nise.nabe@gmail.com>
2012-07-15 14:58:10 +0900

    fixed test to pass using default timezone setting

--- src/notifier/parser/SRMCalendarParserTest.java
+++ src/notifier/parser/SRMCalendarParserTest.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import notifier.SRM;
 
@@ -27,21 +28,22 @@ public class SRMCalendarParserTest {
 	@Test
 	public void printSRMs() throws Exception {
 		System.out.println(Locale.getDefault());
+		TimeZone.setDefault(TimeZone.getTimeZone("en_US"));
 		SRMCalendarParserMock parser = new SRMCalendarParserMock("http://community.topcoder.com/tc?module=Static&d1=calendar&d2=thisMonth");
 		List<SRM> srms = parser.getSRMs();
 
-		String expected1 = "notifier.SRM[name=SRM 548,url=/tc?module=MatchDetails&rd=15170,competisionTime=Tue Jul 03 00:10:00 JST 2012,regiserTime=Mon Jul 02 21:00:00 JST 2012,count=0]";
+		String expected1 = "notifier.SRM[name=SRM 548,url=/tc?module=MatchDetails&rd=15170,competisionTime=Mon Jul 02 15:10:00 GMT 2012,regiserTime=Mon Jul 02 12:00:00 GMT 2012,count=0]";
 		String actual1 = srms.get(0).toString();
-		System.out.println(expected1);
-		System.out.println(actual1);
 		assertEquals(expected1, actual1);
 
-		String expected2 = "notifier.SRM[name=SRM 549,url=/tc?module=MatchDetails&rd=15171,competisionTime=Mon Jul 09 20:10:00 JST 2012,regiserTime=Mon Jul 09 17:00:00 JST 2012,count=0]";
+		String expected2 = "notifier.SRM[name=SRM 549,url=/tc?module=MatchDetails&rd=15171,competisionTime=Mon Jul 09 11:10:00 GMT 2012,regiserTime=Mon Jul 09 08:00:00 GMT 2012,count=0]";
 		String actual2 = srms.get(1).toString();
 		assertEquals(expected2, actual2);
 
-		String expected3 = "notifier.SRM[name=SRM 550,url=/tc?module=MatchDetails&rd=15172,competisionTime=Sun Jul 22 01:00:00 JST 2012,regiserTime=Sat Jul 21 22:00:00 JST 2012,count=0]";
+		String expected3 = "notifier.SRM[name=SRM 550,url=/tc?module=MatchDetails&rd=15172,competisionTime=Sat Jul 21 16:00:00 GMT 2012,regiserTime=Sat Jul 21 13:00:00 GMT 2012,count=0]";
 		String actual3 = srms.get(2).toString();
+		System.out.println(expected3);
+		System.out.println(actual3);
 		assertEquals(expected3, actual3);
 	}
 }
