87bb6d4ad8b08794863ad3cf5483d652019d23cf
nise_nabe <nise.nabe@gmail.com>
2012-07-15 14:00:03 +0900

    fixed test case

--- src/notifier/parser/SRMCalendarParserTest.java
+++ src/notifier/parser/SRMCalendarParserTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.List;
 
 import notifier.SRM;
 
@@ -25,9 +26,21 @@ public class SRMCalendarParserTest {
 	@Test
 	public void printSRMs() throws Exception {
 		SRMCalendarParserMock parser = new SRMCalendarParserMock("http://community.topcoder.com/tc?module=Static&d1=calendar&d2=thisMonth");
-		for(SRM srm : parser.getSRMs()){
-			System.out.println(srm);
-		}
+		List<SRM> srms = parser.getSRMs();
+
+		String expected1 = "notifier.SRM[name=SRM 548,url=/tc?module=MatchDetails&rd=15170,competisionTime=Tue Jul 03 00:10:00 JST 2012,regiserTime=Mon Jul 02 21:00:00 JST 2012,count=0]";
+		String actual1 = srms.get(0).toString();
+		System.out.println(expected1);
+		System.out.println(actual1);
+		assertEquals(expected1, actual1);
+
+		String expected2 = "notifier.SRM[name=SRM 549,url=/tc?module=MatchDetails&rd=15171,competisionTime=Mon Jul 09 20:10:00 JST 2012,regiserTime=Mon Jul 09 17:00:00 JST 2012,count=0]";
+		String actual2 = srms.get(1).toString();
+		assertEquals(expected2, actual2);
+
+		String expected3 = "notifier.SRM[name=SRM 550,url=/tc?module=MatchDetails&rd=15172,competisionTime=Sun Jul 22 01:00:00 JST 2012,regiserTime=Sat Jul 21 22:00:00 JST 2012,count=0]";
+		String actual3 = srms.get(2).toString();
+		assertEquals(expected3, actual3);
 	}
 }
 
