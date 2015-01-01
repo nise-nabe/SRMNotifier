46c1246e9b2202af86e3335d119306263d76002e
nise_nabe <nise.nabe@gmail.com>
2015-01-02 08:37:22 +0900

    not to execute test

--- src/test/java/notifier/TwitterManagerTest.java
+++ src/test/java/notifier/TwitterManagerTest.java
@@ -2,12 +2,14 @@ package notifier;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import twitter4j.TwitterFactory;
 
 public class TwitterManagerTest {
 	@Test
+	@Ignore
 	public void getAccount() throws Exception {
 		String expected = "tc_srm_jp_bot";
 		String actual  = new  TwitterFactory().getInstance().getScreenName();
