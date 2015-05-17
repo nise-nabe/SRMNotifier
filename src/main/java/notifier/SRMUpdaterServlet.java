4f8b231892a53b1ebd9628cde67f1a28c937c891
nise_nabe <nise.nabe@gmail.com>
2015-05-17 13:51:53 +0900

    remove unused import

--- src/main/java/notifier/SRMUpdaterServlet.java
+++ src/main/java/notifier/SRMUpdaterServlet.java
@@ -2,7 +2,6 @@ package notifier;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
