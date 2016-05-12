11157a49055f4f8425c0473c77d0b1f814010d93
nise_nabe <nise.nabe@gmail.com>
2016-05-12 23:06:20 +0900

    140 文字越えてたらツイートできないのでラウンド名をトランケート

--- src/main/java/notifier/SRMNotifierServlet.java
+++ src/main/java/notifier/SRMNotifierServlet.java
@@ -99,11 +99,16 @@ public class SRMNotifierServlet extends HttpServlet {
 	private void post(String msg, SRM srm, String date) throws TwitterException {
 		// Twitter twitter;
 		// SRM 463 終了しました at 2010年03月02日（火） 22時35分 #Topcoder #SRM
-		String status = srm.getName() + " " + msg + " " + date;
+		String status = " " + msg + " " + date;
 		if (2 <= srm.getCount() && srm.getCount() <= 7) {
 			status += " Arena -> http://bit.ly/gloK93 WebArena -> https://arena.topcoder.com/";
 		}
 		status += " " + hash;
+		if ((srm.getName() + status).length() > 140) {
+			status = srm.getName().substring(0, 140 - status.length()) + status;
+		} else {
+			status = srm.getName() + status;
+		}
 		TwitterManager.post(status);
 	}
 
